8ea49d166d9c36aabb342efa313551d12e0cf58b
MotionPhix <kingscrib1@gmail.com>
2025-04-17 15:25:46 +0200

    revamp newsletter component

--- app/Filament/Resources/ContactSubmissionResource.php
+++ app/Filament/Resources/ContactSubmissionResource.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources;
 use App\Filament\Resources\ContactSubmissionResource\Pages;
 use App\Filament\Resources\ContactSubmissionResource\RelationManagers;
 use App\Models\ContactSubmission;
+use App\Notifications\ContactFormResponse;
 use Filament\Forms;
 use Filament\Forms\Form;
 use Filament\Notifications\Notification;
