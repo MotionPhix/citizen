04486d2ac761e31b09b55126a6625731ab312089
MotionPhix <aishootsmo@gmail.com>
2025-06-23 05:56:29 +0200

    changed actions alignment

--- app/Filament/Resources/ImpactMetricResource/Pages/EditImpactMetric.php
+++ app/Filament/Resources/ImpactMetricResource/Pages/EditImpactMetric.php
@@ -5,10 +5,12 @@ namespace App\Filament\Resources\ImpactMetricResource\Pages;
 use App\Filament\Resources\ImpactMetricResource;
 use Filament\Actions;
 use Filament\Resources\Pages\EditRecord;
+use Filament\Support\Enums\Alignment;
 
 class EditImpactMetric extends EditRecord
 {
   protected static string $resource = ImpactMetricResource::class;
+  public static string|\Filament\Support\Enums\Alignment $formActionsAlignment = Alignment::Right;
 
   protected function getHeaderActions(): array
   {
